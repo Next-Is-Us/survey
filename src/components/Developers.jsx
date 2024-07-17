6ce9431c40626e5cdb3c0820d6f837c1010bf912
Jun279 <33.beautifulboy@gmail.com>
2024-07-17 23:53:58 +0900

    update Developer.jsx

--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 import LeftAllow from "../image/allow_left.png";
+import PersonContainer from "./PersonContainer";
+import PmProfileImg from "../image/pmProfile.svg";
+import designerProfileImg from "../image/designerProfile.svg";
+import backProfileK from "../image/backProfileK.svg";
+import backProfileL from "../image/backProfileL.svg";
+import frontProfileL from "../image/frontProfileL.svg";
+import frontProfileP from "../image/frontProfileP.svg";
+import pencilImg from "../image/pencil.svg";
+import shareImg from "../image/share.svg";
 
 const Container = styled.div`
   // background: #ffffff;
@@ -78,7 +87,7 @@ const IntroduceSubText = styled.p`
 const IntroduceTeamTextContainer = styled.div`
   margin-top: 1.25rem;
   width: 100%;
-`
+`;
 
 const IntroduceTeamText = styled.p`
   text-align: left;
@@ -94,7 +103,105 @@ const IntroduceTeamText = styled.p`
   letter-spacing: -0.025rem;
 `;
 
+const PersonInfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  margin-top: 0.5rem;
+`;
+
+const DescribeTextContainer = styled.div`
+  width: 100%;
+  margin-top: 2.5rem;
+`;
+
+const DescribeMainText = styled.p`
+  color: var(--Font-02_black, #111);
+
+  /* MO/Headline/KR/H5_KR_Rg */
+  font-family: Pretendard;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.625rem; /* 144.444% */
+  letter-spacing: -0.02813rem;
+  text-align: left;
+  margin-left: 1.25rem;
+`;
+
+const DescribeSubTextContainer = styled.div`
+  width: 100%;
+  margin-top: 0.5rem;
+  dispaly: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+`;
+
+const DescribeSubText = styled.p`
+  color: var(--Font-03_Gray, #505050);
+
+  /* MO/Body/KR/B2_KR_Rg */
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.25rem; /* 142.857% */
+  letter-spacing: -0.02188rem;
+  text-align: left;
+  margin-left: 1.25rem;
+  margin-right: 1.25rem;
+`;
+
+const ButtonComponentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+  margin-top: 3.75rem;
+`;
+
+const ButtonComponent = styled.button`
+  display: flex;
+  gap: 0.25rem;
+  width: 20rem;
+  padding: 0.875rem 1rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.75rem;
+  color: var(--Font-01_White, #fff);
+  text-align: center;
+  border: none;
+
+  /* Button/BUT1_Sb */
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.5rem; /* 150% */
+  letter-spacing: -0.025rem;
+  background: ${(props) => {
+    if (props.participate) {
+      return '#A30FFA';
+    } else if (props.share) {
+      return 'var(--System-Green-500, #4AA63C)';
+    } else {
+      return 'gray';
+    }
+  }};
+`;
+
 export default function Devlopers() {
+  const personProfile = [
+    { image: PmProfileImg, name: "김지은" },
+    { image: designerProfileImg, name: "권기남" },
+    { image: backProfileK, name: "강민서" },
+    { image: backProfileL, name: "이주연" },
+    { image: frontProfileL, name: "이나경" },
+    { image: frontProfileP, name: "박제준" },
+  ];
   return (
     <Container>
       <Article>
@@ -127,6 +234,44 @@ export default function Devlopers() {
           <IntroduceTeamTextContainer>
             <IntroduceTeamText>[네카라쿠배그다음 우리] 팀원</IntroduceTeamText>
           </IntroduceTeamTextContainer>
+          <PersonInfoContainer>
+            {personProfile.map((item, index) => {
+              return (
+                <PersonContainer
+                  key={index}
+                  image={item.image}
+                  name={item.name}
+                />
+              );
+            })}
+          </PersonInfoContainer>
+          <DescribeTextContainer>
+            <DescribeMainText>
+              현재 서비스 기획을 위해 사용자들의 <br />
+              목소리를 듣고 있습니다.
+            </DescribeMainText>
+          </DescribeTextContainer>
+          <DescribeSubTextContainer>
+            <DescribeSubText>
+              약 3분정도 시간내셔서 아래의 설문조사에 참여해주신다면, <br />
+              신규 서비스에 큰 도움이 됩니다.
+            </DescribeSubText>
+            <DescribeSubText>
+              설문조사 중 개인정보를 받는 항목은 존재하지 않으며,
+              <br />
+              설문 조사 내용은 신규 서비스 기획 후 안전하게 폐기될 예정입니다.
+            </DescribeSubText>
+          </DescribeSubTextContainer>
+          <ButtonComponentContainer>
+            <ButtonComponent participate>
+              <img src={pencilImg} alt="pencilImg" />
+              설문조사 참여하기
+            </ButtonComponent>
+            <ButtonComponent share>
+              <img src={shareImg} alt="pencilImg" />
+              테스트 공유하기
+            </ButtonComponent>
+          </ButtonComponentContainer>
         </PosTitleContainer>
       </Article>
     </Container>
--- src/components/PersonContainer.jsx
+++ src/components/PersonContainer.jsx
@@ -8,8 +8,24 @@ const PersonInfoBox = styled.div`
   gap: 0.25rem;
 `
 
-export default function PersonContainer() {
-  return (
+const PersonNameText = styled.p`
+  color: var(--Font-02_black, #111);
+  text-align: center;
 
+  /* MO/Body/KR/B4_KR_Rg */
+  font-family: Pretendard;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.125rem; /* 150% */
+  letter-spacing: -0.01875rem;
+`
+
+export default function PersonContainer({image, name}) {
+  return (
+    <PersonInfoBox>
+      <img src={image} alt="profileImg" />
+      <PersonNameText>{name}</PersonNameText>
+    </PersonInfoBox>
   )
 }
