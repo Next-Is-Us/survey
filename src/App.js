53c864de83a16bbd15c8406f5e99b02bf3333683
이나경 <leena3823@gmail.com>
2024-07-19 11:46:51 +0900

    Update Preload logic

--- src/App.js
+++ src/App.js
@@ -10,7 +10,35 @@ import Question from './components/Question';
 import Result from './components/Result';
 import Devlopers from './components/Developers';
 
+import PmProfileImg from './image/pmProfilep.png';
+import designerProfileImg from './image/designerProfile.jpeg';
+import backProfileK from './image/backProfileK.jpeg';
+import backProfileL from './image/backProfileL.jpeg';
+import frontProfileL from './image/frontProfileL.png';
+import frontProfileP from './image/frontProfileP.png';
+import Frame from './image/mainIcon.svg';
+
+function preloadImages() {
+  const images = [
+    PmProfileImg,
+    designerProfileImg,
+    backProfileK,
+    backProfileL,
+    frontProfileL,
+    frontProfileP,
+    Frame,
+  ];
+  images.forEach((image) => {
+    const img = new Image();
+    img.src = image;
+  });
+}
+
 function App() {
+  useEffect(() => {
+    preloadImages();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
